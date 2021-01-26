700e2e902d28904a27c688a1c7c988a30dc2608a
JZacharyL <47034052+JZacharyL@users.noreply.github.com>
2021-01-26 09:30:47 +0800

    added stuff to ai huntable

--- AGILA/Source/AGILA/AI_controller_Huntable.h
+++ AGILA/Source/AGILA/AI_controller_Huntable.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "C_HuntablePawn.h"
 #include "AI_controller_Huntable.generated.h"
+
 
 /**
  * 
@@ -13,8 +15,11 @@ UCLASS()
 class AGILA_API AAI_controller_Huntable : public AAIController
 {
 	GENERATED_BODY()
+protected:
+	UPROPERTY(BlueprintReadWrite) AActor* actorCopy;
 private:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaSeconds) override;
-	
+	AC_HuntablePawn* GetControlledHuntable() const;
+	//AC_HuntablePawn* GetPlayer() const; replace AC_HUntable with player pawn
 };
